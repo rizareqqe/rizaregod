87045b44a5ef8f28d3d28451635708fec1025b8e
nt <macjluna25@gmail.com>
2025-02-21 20:57:33 +0300

    Добавленны программы с циклами

---
+++ 2.2.php
@@ -0,0 +1,25 @@
+<?php
+
+$limit = 100;
+
+echo "Простые числа от 1 до $limit:";
+
+for ($n = 2; $n <= $limit; $n++) {
+
+ $b = true;
+
+ for ($a = 2; $a < $n; $a++) {
+
+  if ($n % $a == 0) {
+
+   $b = false;
+
+   break;
+  }
+ }
+
+ if ($b) {
+
+  echo $n . " ";
+ }
+}
